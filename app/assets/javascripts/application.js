b4940e22c6b32d154bbc4d4345c9966bea9d493c
Thomas Korsgaard Schmidt <ts@kruby.dk>
2014-02-04 22:03:06 +0100

    jquery.turbolinks to the resque

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,11 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-
-// Loads all Bootstrap javascripts
-//= require bootstrap
-
-
+//= require jquery.turbolinks
 //= require fancybox
+//= require jquery_ujs
+//= require_tree .
+//= require bootstrap
+//= require turbolinks
+
+
