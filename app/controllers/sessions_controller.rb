d672953efa80f981717a4405098eb51d45639af1
kruby <ts@kruby.dk>
2014-04-16 15:46:36 +0200

    Lidt flere rettelser vedr. assets

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,33 @@
 class SessionsController < ApplicationController
 
-  def new
-  end
+	def new
+	end
 
-  def create
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      @partner = Partner.find_by_user_id(user.id)
-      if @partner != nil
-        session[:partner_id] = @partner.id
-        redirect_to :controller => 'contents', :action => 'index'
-      else
-        redirect_to :controller => session[:current_controller] || 'contents'
-      end
-        session[:current_controller] = nil
-        flash[:notice] = "Du er nu logget ind!"
-       #:notice => "Logged in!"
-    else
-      flash[:notice] = "Forkert password eller email"
-      render "new"
-    end
-  end
+	def create
+		user = User.authenticate(params[:email], params[:password])
+		if user
+			session[:user_id] = user.id
+			@partner = Partner.find_by_user_id(user.id)
+			if @partner != nil
+				session[:partner_id] = @partner.id
+				redirect_to :controller => 'posts', :action => 'index'
+			else
+				redirect_to :controller => session[:current_controller] || 'posts'
+			end
+			session[:current_controller] = nil
+			flash[:notice] = "Du er nu logget ind!"
+			#:notice => "Logged in!"
+		else
+			flash[:notice] = "Forkert password eller email"
+			render "new"
+		end
+	end
 
-  def destroy
-    session[:user_id] = nil
-    session[:partner_id] = nil
-    redirect_to root_url, :notice => "Du er nu logget ud!"
-  end
+	def destroy
+		session[:user_id] = nil
+		session[:partner_id] = nil
+		session[:current_controller] = nil
+		redirect_to root_url, :notice => "Du er nu logget ud!"
+	end
 
 end
